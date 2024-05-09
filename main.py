76b7003bf51498d0eb23cc7f65ab1ba1a8e26390
galytska <galytska13@gmail.com>
2024-05-09 12:48:56 +0300

    add calcul, save methods

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import os.path
+
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, \
-    QCheckBox, QTreeView, QPushButton, QHBoxLayout, QVBoxLayout, QMainWindow
-from PyQt5.QtGui import QStandardItemModel
+    QCheckBox, QTreeView, QPushButton, QHBoxLayout, QVBoxLayout, QMainWindow, QMessageBox, QFileDialog
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 
 class FinanceApp(QMainWindow):
@@ -18,9 +22,14 @@ class FinanceApp(QMainWindow):
         self.year_total_treeview = QTreeView()
         self.year_total_treeview.setModel(self.model)
         self.calculate_button = QPushButton('Calculate')
+        self.calculate_button.clicked.connect(self.calculate)
         self.reset_button = QPushButton('Reset')
+        self.reset_button.clicked.connect(self.reset_input)
         self.save_button = QPushButton('Save')
-        self.figure = QLabel('------Here wille the plot------')
+        self.save_button.clicked.connect(self.save_date)
+        self.save_button.hide()
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
 
         master_layout = QVBoxLayout()
         row1 = QHBoxLayout()
@@ -41,13 +50,69 @@ class FinanceApp(QMainWindow):
         column1.addWidget(self.calculate_button)
         column1.addWidget(self.reset_button)
         column1.addWidget(self.save_button)
-        row2.addLayout(column1, 20)
-        column2.addWidget(self.figure)
-        row2.addLayout(column2, 80)
+        row2.addLayout(column1, 30)
+        column2.addWidget(self.canvas)
+        row2.addLayout(column2, 70)
         master_layout.addLayout(row2)
 
         main_window.setLayout(master_layout)
         self.setCentralWidget(main_window)
+
+    def calculate(self):
+        try:
+            init_invest = float(self.init_invest_lineedit.text())
+            interest_rate = float(self.interest_rate_lineedit.text())
+            num_of_years = int(self.num_of_years_lineedit.text())
+        except ValueError:
+            QMessageBox.warning(self, 'Error', 'Invalid input Please Enter a valid number')
+            return
+        else:
+            total = init_invest
+            for year in range(1, num_of_years + 1):
+                total += total * (interest_rate / 100)
+                item_year = QStandardItem(str(year))
+                item_total = QStandardItem(f'{total:.2f}')
+                self.model.appendRow([item_year, item_total])
+
+            self.figure.clear()
+            ax = self.figure.subplots()
+            years = list(range(1, num_of_years + 1))
+            totals = [init_invest * (1 + interest_rate / 100) ** year for year in years]
+
+            ax.plot(years, totals)
+            ax.set_title("Interest Chart")
+            ax.set_xlabel("Year")
+            ax.set_ylabel("Total")
+            self.canvas.draw()
+
+            self.save_button.show()
+
+    def reset_input(self):
+        self.init_invest_lineedit.clear()
+        self.interest_rate_lineedit.clear()
+        self.num_of_years_lineedit.clear()
+        self.model.clear()
+
+        self.figure.clear()
+        self.canvas.draw()
+
+    def save_date(self):
+        dir_path = QFileDialog.getExistingDirectory(self, 'Save directory')
+        if dir_path:
+            save_dir_path = os.path.join(dir_path, 'Saved')
+            os.makedirs(save_dir_path, exist_ok=True)
+            save_file_path = os.path.join(save_dir_path, 'result_finance.csv')
+            with open(save_file_path, 'w') as file:
+                file.write('Year,Total\n')
+                for row in range(self.model.rowCount()):
+                    year = self.model.index(row, 0).data()
+                    total = self.model.index(row, 1).data()
+                    file.write(f'{year},{total}\n')
+
+            plt.savefig(os.path.join(save_dir_path, 'canvas.png'))
+            QMessageBox.information(self, 'Save result', 'Results saved to your folder')
+        else:
+            QMessageBox.warning(self, 'Save result', 'Saved directory not selected')
 
 
 if __name__ == '__main__':
